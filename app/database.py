bb7bfa0c4f7eb88e811f98bd89bc05723eedcbe4
Saidazim0323 <150612742+Saidazim0323@users.noreply.github.com>
2026-01-31 12:14:30 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,8 +2,18 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = (os.getenv("DATABASE_URL") or "").strip()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
-Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+    pool_pre_ping=True
+)
+
+Session = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
 Base = declarative_base()
